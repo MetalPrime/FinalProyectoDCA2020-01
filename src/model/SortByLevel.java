d329996493528014343449118d78833838218e02
Jhon Garcia <metalprime57@gmail.com>
2020-05-18 02:21:40 -0500

    sort pokemon

---
+++ src/model/SortByLevel.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Comparator;
+
+public class SortByLevel implements Comparator<Pokemon>{
+
+
+	@Override
+	public int compare(Pokemon o1, Pokemon o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/model/SortByType.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Comparator;
+
+public class SortByType implements Comparator<Pokemon>{
+
+	@Override
+	public int compare(Pokemon o1, Pokemon o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
